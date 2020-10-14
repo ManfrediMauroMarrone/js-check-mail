949b070cf6e196c66ababc961b89efb3a72232a0
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-14 16:10:51 +0200

    base esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var user_email = prompt('Inserisci il tuo indirizzo email');
+
+var email_list = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
+
+for (var i = 0; i < array.length; i++) {
+    array[i]
+}
