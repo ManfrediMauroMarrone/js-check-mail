ebe6a5cefb5e0c22cae6fb2a91704761d257cd33
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-14 21:38:24 +0200

    esercizio completato

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ var user_email = prompt('Inserisci il tuo indirizzo email');
 
 var email_list = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
 
+var found = false;
+
 
 for(var i = 0; i < email_list.length; i++) {
 
     var mail_giuste = email_list[i]
 
     if (user_email == mail_giuste) {
+        found = true
         console.log('la tua email è corretta');
     }
 
 }
 
-if (user_email != mail_giuste) {
+if (found == false) {
     console.log('la tua email NON è corretta');
 }
