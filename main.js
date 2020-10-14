c9c21a409ef43c9cab6e593cbdac7afa686ba354
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-14 18:51:42 +0200

    aggiunto modifiche

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var user_email = prompt('Inserisci il tuo indirizzo email');
 
 var email_list = ['pippo@gmail.com', 'pluto@gmail.com', 'paperino@gmail.com'];
 
-for (var i = 0; i < array.length; i++) {
-    array[i]
+
+for(var i = 0; i < email_list.length; i++) {
+
+    var mail_giuste = email_list[i]
+
+    if (user_email == mail_giuste) {
+        console.log('la tua email è corretta');
+    }
+
+}
+
+if (user_email != mail_giuste) {
+    console.log('la tua email NON è corretta');
 }
